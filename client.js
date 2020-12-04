5b90dbc7871ba8a6bcd8a67ed164f3ba6cc3b370
Sebastian Clancy <foldit.com@gmail.com>
2020-12-03 20:10:26 -0800

    Changes to ui, added start of a menu class

--- client.js
+++ client.js
@@ -1,11 +1,57 @@
-function init(){
+function init() {
     let canvas = document.getElementById("canvas")
     canvas.height = window.innerHeight - 0
     canvas.width = window.innerWidth - 200
-    window.addEventListener('resize',(e)=>{
+    window.addEventListener('resize', (e) => {
         canvas.height = window.innerHeight - 0
         canvas.width = window.innerWidth - 200
     })
-    
 }
 init()
+class Menu {
+    topLevelItems = []
+    secondaryDetails = []
+    constructor() {
+        // Assume age is zero, start with the basic options. 
+        this.topLevelItems = ["Population", "Resources"]
+        this.secondaryDetails = [
+            {
+                title: "Assign your population to jobs",
+                choices: {
+                    "Hunter": null,
+                    "Gatherer": null
+                }
+            },
+            {
+                title: "Manage your resources here",
+                choices: {
+                    "Rocks": null,
+                    "Wood": null,
+                    "Food": null
+                }
+            }
+        ]
+    }
+    getTopElements() {
+        let elements = []
+        this.topLevelItems.forEach(item => {
+            let element = document.createElement("ul")
+            element.innerHTML = item
+            element.onclick = this.buttonClicked
+            elements.push(element)
+        })
+        return elements
+    }
+    buttonClicked(e){
+        console.log(e)
+    }
+    getDetails(name) {
+        let index = this.topLevelItems.indexOf(name)
+        if (index == -1) {
+            return None
+        }
+        let item = this.secondaryDetails[index]
+        let element = document.createElement("div")
+        element.innerHTML
+    }
+}
